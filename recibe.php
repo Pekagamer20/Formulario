65515d39a89904138c1a4af31252d88e40054917
Pekagamer20 <tanjiroslayer004@gmail.com>
2026-09-09 22:50:45 -0600

    resolucion de errores en formulario y recibe

--- recibe.php
+++ recibe.php
@@ -3,14 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Respuestas</title>
 </head>
 <body>
     <?php 
     if($_SERVER["REQUEST_METHOD"] == "POST") { 
         $nombre = $_POST["nombre"];
-        $correo = $_POST["correo"];
+        $correo = $_POST["email"];
         
+        //limpieza 
+        // DB
+
+        echo "<h2> Datos recibidos: </h2>";
+        echo "<p> Nombre: " . $nombre . "</p>";
+        echo "<p> Correo: " . $correo . "</p>";
+    }else {
+        echo "<p> no es una peticion tipo POST. </p>";
     }
     ?> 
 </body>
